04450d421d12601c46d6d57d240507820d47e83f
Dom Rutili <domrutili@Doms-MacBook-Pro.local>
2022-07-06 15:05:54 +1000

    leonardo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,263 @@
+import {
+  Box,
+  //Button,
+  CssBaseline,
+  Divider,
+  Link,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from '@mui/material';
+import {
+  //ActionRequest,
+  //AudioActionResponse,
+  ChatController,
+  //FileActionResponse,
+  MuiChat,
+} from 'chat-ui-react';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const muiTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#d5654d',
+    },
+  },
+});
+
+export function App(): React.ReactElement {
+  const [chatCtl] = React.useState(
+    new ChatController({
+      showDateTime: true,
+    }),
+  );
+
+  React.useMemo(() => {
+    echo(chatCtl);
+  }, [chatCtl]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={muiTheme}>
+      <CssBaseline />
+      <Box sx={{ height: '100%', backgroundColor: 'gray' }}>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            height: '100%',
+            maxWidth: '640px',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            bgcolor: 'background.default',
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Typography sx={{ p: 1 }}>
+            Welcome to{' '}
+            <Link href="https://wpopera.co/leonardo/">
+              LEONARDO
+            </Link>{' '}
+            .
+          </Typography>
+          <Divider />
+          <Box sx={{ flex: '1 1 0%', minHeight: 0 }}>
+            <MuiChat chatController={chatCtl} />
+          </Box>
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
 }
 
-export default App;
+async function fetchJARVIS(text: string): Promise<string> {
+  let response = await fetch('https://integrationhub.wpopera.cloud/webhook/generate?topic=' + text);
+  return await response.text();
+}
+
+async function echo(chatCtl: ChatController): Promise<void> {
+  // await chatCtl.addMessage({
+  //   type: 'text',
+  //   content: `Please enter something.`,
+  //   self: false,
+  //   avatar: '-',
+  // });
+  const text = await chatCtl.setActionRequest({
+    type: 'text',
+    placeholder: 'Please ask me something',
+    sendButtonText:"Chat",
+    onClick:()=>{alert("clicked")}
+  });
+  const response = await fetchJARVIS(text.value);
+  await chatCtl.addMessage({
+    type: 'Send',
+    content: `${response.trim()}`,
+    self: false,
+    avatar: 'face.png',
+  });
+
+  // await chatCtl.addMessage({
+  //   type: 'text',
+  //   content: `What is your gender?`,
+  //   self: false,
+  //   avatar: '-',
+  // });
+  // const sel = await chatCtl.setActionRequest({
+  //   type: 'select',
+  //   options: [
+  //     {
+  //       value: 'man',
+  //       text: 'Man',
+  //     },
+  //     {
+  //       value: 'woman',
+  //       text: 'Woman',
+  //     },
+  //     {
+  //       value: 'other',
+  //       text: 'Other',
+  //     },
+  //   ],
+  // });
+  // await chatCtl.addMessage({
+  //   type: 'text',
+  //   content: `You have selected ${sel.value}.`,
+  //   self: false,
+  //   avatar: '-',
+  // });
+
+  // await chatCtl.addMessage({
+  //   type: 'text',
+  //   content: `What is your favorite fruit?`,
+  //   self: false,
+  //   avatar: '-',
+  // });
+  // const mulSel = await chatCtl.setActionRequest({
+  //   type: 'multi-select',
+  //   options: [
+  //     {
+  //       value: 'apple',
+  //       text: 'Apple',
+  //     },
+  //     {
+  //       value: 'orange',
+  //       text: 'Orange',
+  //     },
+  //     {
+  //       value: 'none',
+  //       text: 'None',
+  //     },
+  //   ],
+  // });
+  // await chatCtl.addMessage({
+  //   type: 'text',
+  //   content: `You have selected '${mulSel.value}'.`,
+  //   self: false,
+  //   avatar: '-',
+  // });
+
+  // await chatCtl.addMessage({
+  //   type: 'text',
+  //   content: `What is your favorite picture?`,
+  //   self: false,
+  //   avatar: '-',
+  // });
+  // const file = (await chatCtl.setActionRequest({
+  //   type: 'file',
+  //   accept: 'image/*',
+  //   multiple: true,
+  // })) as FileActionResponse;
+  // await chatCtl.addMessage({
+  //   type: 'jsx',
+  //   content: (
+  //     <div>
+  //       {file.files.map((f) => (
+  //         <img
+  //           key={file.files.indexOf(f)}
+  //           src={window.URL.createObjectURL(f)}
+  //           alt="File"
+  //           style={{ width: '100%', height: 'auto' }}
+  //         />
+  //       ))}
+  //     </div>
+  //   ),
+  //   self: false,
+  //   avatar: '-',
+  // });
+
+  // await chatCtl.addMessage({
+  //   type: 'text',
+  //   content: `Please enter your voice.`,
+  //   self: false,
+  //   avatar: '-',
+  // });
+  // const audio = (await chatCtl
+  //   .setActionRequest({
+  //     type: 'audio',
+  //   })
+  //   .catch(() => ({
+  //     type: 'audio',
+  //     value: 'Voice input failed.',
+  //     avatar: '-',
+  //   }))) as AudioActionResponse;
+  // await (audio.audio
+  //   ? chatCtl.addMessage({
+  //       type: 'jsx',
+  //       content: (
+  //         <a href={window.URL.createObjectURL(audio.audio)}>Audio downlaod</a>
+  //       ),
+  //       self: false,
+  //       avatar: '-',
+  //     })
+  //   : chatCtl.addMessage({
+  //       type: 'text',
+  //       content: audio.value,
+  //       self: false,
+  //       avatar: '-',
+  //     }));
+
+  // await chatCtl.addMessage({
+  //   type: 'text',
+  //   content: `Please press the button.`,
+  //   self: false,
+  //   avatar: '-',
+  // });
+  // const good = await chatCtl.setActionRequest({
+  //   type: 'custom',
+  //   Component: GoodInput,
+  // });
+  // await chatCtl.addMessage({
+  //   type: 'text',
+  //   content: `You have pressed the ${good.value} button.`,
+  //   self: false,
+  //   avatar: '-',
+  // });
+
+  echo(chatCtl);
+}
+
+// function GoodInput({
+//   chatController,
+//   actionRequest,
+// }: {
+//   chatController: ChatController;
+//   actionRequest: ActionRequest;
+// }) {
+//   const chatCtl = chatController;
+
+//   const setResponse = React.useCallback((): void => {
+//     const res = { type: 'custom', value: 'Good!' };
+//     chatCtl.setActionResponse(actionRequest, res);
+//   }, [actionRequest, chatCtl]);
+
+//   return (
+//     <div>
+//       <Button
+//         type="button"
+//         onClick={setResponse}
+//         variant="contained"
+//         color="primary"
+//       >
+//         Good!
+//       </Button>
+//     </div>
+//   );
+// }
